73d3be8c27bcd8a12c8e9e751321e4d20bf57b07
Lukas.J.Han <lukas.j.han@gmail.com>
2024-01-21 12:30:13 +0900

    fix: contact msg

--- components/pages/contact.tsx
+++ components/pages/contact.tsx
@@ -1,13 +1,15 @@
-import ContactForm from "@/components/pages/contact-form"
-import HeadingText from "@/components/heading-text"
+import ContactForm from "@/components/pages/contact-form";
+import HeadingText from "@/components/heading-text";
 
 export default function Contact() {
   return (
     <main className="container flex flex-col items-center py-8" id="contact">
       <div className="flex flex-col items-center space-y-2 text-center">
-        <HeadingText subtext="Contact for usage & pricing">Contact</HeadingText>
+        <HeadingText subtext="Send us a message to get in touch with the team">
+          Contact
+        </HeadingText>
       </div>
       <ContactForm />
     </main>
-  )
+  );
 }
